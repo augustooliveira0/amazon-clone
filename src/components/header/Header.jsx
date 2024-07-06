d624e80cf66399e4858bbf8eef9e7e1113d2d10f
augustooliveira0 <soninclovis@gamil.com>
2024-07-06 19:10:01 -0300

    config header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-
+import React from "react";
+import { logo } from "../../assets/index";
 const Header = () => {
   return (
-    <div className='w-full bg-amazon_blue text-white px-4 py-3 '>
+    <div className="w-full bg-amazon_blue text-white px-4 py-3 ">
+
+      <div className="px-2 h-[80%] flex items-center border border-transparent 
+      hover:border-white cursor-pointer duration-100">
+        <img className="w-24 mt-2" src={logo} alt="logo" />
+      </div>
 
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
