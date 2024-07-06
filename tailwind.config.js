76dd6def8ee5fbfa106f82b13bd94cb040f5f821
augustooliveira0 <soninclovis@gamil.com>
2024-07-06 18:45:43 -0300

    style config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,23 @@ export default {
     extend: {
       maxWidth:{
         container:'1440px'
+      },
+      fontFamily:{
+        titleFont: "Roboto",
+        bodyFont: "Poppins"
+      },
+      colors:{
+        amazon_blue: "#131921",
+        amazon_light: '#232F3E',
+        amazon_yellow: '#febd69',
+        witeText: '#ffffff',
+        ligtText: '#ccc',
+        quantity_box: '#F0F2F2',
+        footerBottom: '#131A22'
+      },
+      boxShadow:{
+        testShadow: '0px 0px 32px 1px rgba(199, 199, 199, 1)',
+        amazonInput: '0 0 3px 2px rgb(228 121 17 / 50%)',
       }
     },
   },
