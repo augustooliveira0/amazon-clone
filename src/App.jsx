07d6d782a4a15730229cfe7cb955688ad231bb20
augustooliveira0 <soninclovis@gamil.com>
2024-07-06 13:03:47 -0300

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import React from "react";
+import Header from "./components/header/Header";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Header/>
   )
 }
